ae7e381ca6e9752726d9e0b28baa59090d6c3411
Rudolf Tisoň <148339600+ruddpj@users.noreply.github.com>
2024-08-08 00:08:22 +0200

    recoded the answer pop up, added a feature to show the correct answer
    made the question appear in the root window
    added a show answer and close buttons
    remade the file tree so each topic has a separate txt file

--- djepardy.py
+++ djepardy.py
@@ -10,59 +10,71 @@ c_text = "yellow"
 f_size = "Arial 32"
 
 cat = []
-file_1 = open("djepardy_q.txt", "r", encoding="utf8")
+file_1 = open("djepardy_themes.txt", "r", encoding="utf8")
 for k in range(6):
     o = file_1.readline()
     cat.append(o)
 file_1.close()
 
 
+def button_sel(get, cc, aq):
+    a = get.x
+    b = get.y
+    if 870 <= a <= 1050:
+        if 800 <= b <= 860:
+            cc.delete("all")
+            Misc.lift(canvas)
+        elif 900 <= b <= 960:
+            cc.create_text(960, 600, text=aq, fill="yellow", font="Arial 24")
+            
+
 def opener(ans, t):
-    file = open("djepardy_q.txt", "r", encoding="utf8")
-    answer = file.readlines()[ans]
+    file = open(f"questions/{ans+1}.txt", "r", encoding="utf8")
+    arr = file.readlines()
     file.close()
-    new = Toplevel(root)
-    new.geometry("600x600")
-    text = Text(new, bg="blue", wrap=WORD, font="Arial 42", fg="yellow")
-    text.insert(INSERT, answer)
-    text.pack()
-    canvas.itemconfig(f"box{t}", fill="gray")
-    canvas.itemconfig(f"text{t}", fill="silver")
+    qtion = arr[t*2]
+    answer = arr[t*2+1]
+    new = Canvas(width=1920, height=1080, bg="blue")
+    new.place(x=0, y=0)
+    new.create_rectangle(870, 800, 1050, 860, fill="yellow", outline="black")
+    new.create_text(960, 830, text="CLOSE", fill="blue", font=f_size)
+    new.create_rectangle(870, 900, 1050, 960, fill="yellow", outline="black")
+    new.create_text(960, 930, text="SHOW", fill="blue", font=f_size)
+    new.create_text(960, 300, text=qtion, fill="yellow", font="Arial 24")
+    canvas.itemconfig(f"box{ans}{t}", fill="gray")
+    canvas.itemconfig(f"text{ans}{t}", fill="silver")
+    new.bind("<Button-1>", lambda event: button_sel(event, new, answer))
 
 
 def cords(get):
     a = get.x
     b = get.y
-    if a in range(0, 320):
-        c = 0
-    elif a in range(321, 640):
-        c = 1
-    elif a in range(640, 960):
-        c = 2
-    elif a in range(961, 1280):
-        c = 3
-    elif a in range(1281, 1600):
-        c = 4
-    else:
-        c = 5
-    question(c, b)
-
-
-def question(m, n):
-    if n > 99:
-        if n in range(100, 280):
-            p = 0
-        elif n in range(281, 460):
-            p = 1
-        elif n in range(461, 640):
-            p = 2
-        elif n in range(641, 820):
-            p = 3
+    if b > 100:
+        if 0 < a <= 320:
+            c = 0
+        elif 320 < a <= 640:
+            c = 1
+        elif 640 < a <= 960:
+            c = 2
+        elif 960 < a <= 1280:
+            c = 3
+        elif 1280 < a <= 1600:
+            c = 4
         else:
-            p = 4
-        u = (m*5)+p+6
-        s = f"{m}{p}"
-        opener(u, s)
+            c = 5
+
+        if 100 < b <= 280:
+            d = 0
+        elif 280 < b <= 460:
+            d = 1
+        elif 460 < b <= 640:
+            d = 2
+        elif 640 < b <= 720:
+            d = 3
+        else:
+            d = 4
+            
+        opener(c, d)
 
 
 for i in range(6):
